10994c1e4e29ff45b7fb46be034aa5ad0084501a
Kai Wells <support@kaiwells.me>
2017-09-06 23:22:46 -0700

    Pagination

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,20 @@ var App = function(searchOutlet, savedSearchesOutlet, resultsOutlet) {
                 self.resultsTitle.text(response.message);
                 return;
             }
+
+            if (response.prevPage) {
+                $("#prevBtn").removeClass("btn-secondary").addClass("btn-primary").click(function() {
+                    self.resetDisplay();
+                    response.prevPage.get(self.displayImages("fixedWidth"));
+                });
+            }
+            if (response.nextPage) {
+                $("#nextBtn").removeClass("btn-secondary").addClass("btn-primary").click(function() {
+                    self.resetDisplay();
+                    response.nextPage.get(self.displayImages("fixedWidth"));
+                });
+            }
+
             for (var i = 0; i < response.images.length; i++) {
                 var img = response.images[i];
                 var imgEl = $("<img class='m-1 imgResult'>")
@@ -35,19 +49,27 @@ var App = function(searchOutlet, savedSearchesOutlet, resultsOutlet) {
                 imgEl.click(function() {
                     var originalURL = $(this).data("saved").original.url;
                     $("#originalImage").attr("src", originalURL);
-                })
+                });
             }
         }
     };
 
+    this.resetDisplay = function() {
+        $(".imgResult").remove();
+        $("#prevBtn").prop("onclick", null).off("click");
+        $("#nextBtn").prop("onclick", null).off("click");
+        $("#prevBtn").removeClass("btn-primary").addClass("btn-secondary");
+        $("#nextBtn").removeClass("btn-primary").addClass("btn-secondary");
+    }
+
     this.handleSearch = function() {
         var query = self.searchInput.val();
-        self.resultsTitle.text("Results for '" + query + "'");
-        $(".imgResult").remove();
-        new GiphyRequest().search(query).get(self.displayImages("fixedWidth"));
+        self.resetDisplay();
+        $("#resultsTitle").text("Results for '" + query + "'");
+        new GiphyRequest().search(query).limit(18).get(self.displayImages("fixedWidth"));
     };
 
     this.resultsOutlet = resultsOutlet;
-    this.resultsTitle = $("<h3>").text("Trending").appendTo(this.resultsOutlet);
-    new GiphyRequest().trending().get(self.displayImages("fixedWidth"));
+    $("#resultsTitle").text("Trending");
+    new GiphyRequest().trending().limit(18).get(self.displayImages("fixedWidth"));
 };
